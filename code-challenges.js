9e1629d31f7ee94be27fa3e292d38f2d3cd3090a
MFDD19D <judas.sanchez1@yahoo.com>
2024-02-14 13:01:02 -0800

    finished

--- code-challenges.js
+++ code-challenges.js
@@ -9,36 +9,68 @@
 
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in two strings and evaluates which of the strings has more characters. Use the test variables provided.
 
-const fruit1 = "apple"
-const fruit2 = "banana"
-// Expected outcome: "banana"
+// const fruit1 = "apple"
+// const fruit2 = "banana"
+// // Expected outcome: "banana"
 
-const fruit3 = "cherry"
-const fruit4 = "kiwi"
-// Expected outcome: "cherry"
+// const fruit3 = "cherry"
+// const fruit4 = "kiwi"
+// // Expected outcome: "cherry"
 
 // Explain your code:
 
 // --------------------1) Create a function that takes in an array of numbers and returns the sum of all the numbers. Use the test variables provided below.
 
-const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
-// Expected output: 15
-const padres1998WorldSeriesRuns = [6, 3, 5, 3]
-// Expected output: 17
+// const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
+// // Expected output: 15
+// const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+// // Expected output: 17
 
-// Explain your code:
+// function calculateSum(numbers) {
+//         // declaring my function that takes in numbers in the array 
+//     let num1 = 0
+//         // we set our variable to num1 with the value placement at 0 which holds the sum within the array
+//     for ( let i = 0; i < numbers.length; i++) {
+//         //i went with a for loop casue its the only one that made the most sense. from within the array we are starting at index 0 within the array of numbers. we use the .legthn to go through the array of all numbers inside.
+//         num1 += numbers[i]
+//         // num one is our place value of where we will start at within the array. they will all add up
 
-// --------------------2) Create a function that takes in a string and  returns the string with all the characters in reversed order. Use the test variable provided below.
+//     }
+//     return num1
+//     // this returns the sum of our funtion 
 
-const lesMiserables = "Jean Valjean"
-// Expected output: "naejlaV naeJ"
+// }
+// console.log(calculateSum(padres1984WorldSeriesRuns))
+// // now were calling our function to add upp all the values within the array of padres1984WorldSeriesRuns
+// console.log(calculateSum(padres1998WorldSeriesRuns))
+// // Explain your code:
 
-const lionKing = "Simba"
-// Expected output: "abmiS"
+// // --------------------2) Create a function that takes in a string and  returns the string with all the characters in reversed order. Use the test variable provided below.
 
-// Explain your code:
+// const lesMiserables = "Jean Valjean"
+// // Expected output: "naejlaV naeJ"
 
-// --------------------3) Create a function that takes in an array and returns an array with only the items from the third, fourth, and fifth indexes. Use the test variable provided below.
+// const lionKing = "Simba"
+// // Expected output: "abmiS"
+// // // Explain your code:
+
+// const reveWords = (words) => {
+//     // we are creating our function name for reversing words within the array 
+//     const splitWords = words.split('') 
+//     // we are creating a function called splitWords that is going into the array and is splitting them to bei individually within the array 
+//     const reversedLetters = splitWords.reverse()
+//     // now that we have our words split individually within the array, we can call on the function .reverse method to reverse the letters within the array
+//     const joinLetters = reversedLetters.join("")
+//     // now that the characters are reversed, we create a function to join them with the .join('') (make sure we have the takcs together or else it will do a different funtions)
+//     return joinLetters
+//     // were returning pour whole function into one with the finished result of all characters split, reversed and joined giving us our desired outcome
+// }
+
+// console.log(reveWords(lesMiserables))
+//     // calling up our words 
+// console.log(reveWords(lionKing))
+
+// // --------------------3) Create a function that takes in an array and returns an array with only the items from the third, fourth, and fifth indexes. Use the test variable provided below.
 
 const stockExchange = [13, 34, -4, 42, 5, -5, 10, 27, 42, 10]
 // Expected output: [42, 5, -5]
@@ -46,4 +78,15 @@ const stockExchange = [13, 34, -4, 42, 5, -5, 10, 27, 42, 10]
 const cohortList = ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot"]
 // Expected output: ["Delta", "Echo", "Foxtrot"]
 
-// Explain your code:
+// // Explain your code:
+const selectItems = function(array) {
+    //were declaring our variable and and calling it selectItems, we use the function array to define a anonymous function that should take the array as an argument and assigns a constant variable selectitems.
+
+    return array.slice(3, 6)
+    // we use the slice method to find only the third through the 5th index of the array of both stock exchange and cohort list?
+}
+
+
+console.log(selectItems(stockExchange))
+// we log it the select items agains the stock exchange to make sure we get teh values within the index we need. 
+console.log(selectItems(cohortList));
--- interview-questions.md
+++ interview-questions.md
@@ -6,7 +6,7 @@ First, without external resources. Challenge yourself to answer from memory as i
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
-Type your initials here to confirm you read the instructions:
+Type your initials here to confirm you read the instructions: js
 
 INSTRUCTOR EXAMPLE: What is a conditional statement?
 
@@ -14,34 +14,58 @@ Your answer:
 
 Researched answer:
 
-1. What is git? What is the difference between git and Github?
+<!-- 1. What is git? What is the difference between git and Github? -->
 
-Your answer:
+<!-- Your answer: 
+like a database for your code to maintain and see cahnges as it grows, as the code is being used by multiple coders --> 
 
-Researched answer:
+<!-- git is the main distributin warehouse of the code while github you can create repositories and can be beneficial with teams and project management -->
 
-2. What is the difference between an array's index and its value?
 
-Your answer:
+<!-- Researched answer: 
 
-Researched answer:
+version control system through repositories and making updates within your code through time and how its being used.   -->
+<!-- Git is the version control system itself, while GitHub is a platform that hosts Git repositories and provides additional collaboration and project management features on top of Git.  -->
+
+<!-- ///////////////////////////////////////// -->
+
+ <!-- 2. What is the difference between an array's index and its value?   -->
+
+<!-- Your answer: 
+an array is a special type of object used for storing multiple values in a single variable. 
+
+index is the placeholder within the array starting in th zero postion 
+var array = [index0, index1, index2, index3]the array is waht holds the data that is going to be tested upon.  -->
+ 
+<!-- Researched answer: -->
+<!-- the index of an array identifies the position of an element within the array, while the value represents the actual data stored at that position. The combination of index and value allows programmers to access and manipulate the elements stored in an array efficiently. -->
+
+ 
+
+<!-- ///////////////////////////////////////////////////// -->
 
 3. Compare and contrast arrays and strings. Please include ways in which they are the same and ways in which they are different.
-
+<!-- 
 Your answer:
 
+ arrays store a range of data to use in to accees to information within the index. string are the number of characters within the array  -->
+
 Researched answer:
+
+<!-- ////////////////////////////////////////////////////////// -->
 
 4. STRETCH: What are the four pillars of object oriented programming?
 
-Researched answer:
+Researched answer:Encapsulation, Inheritance and Polymorphism, and Abstraction
+
+<!-- ////////////////////////////////////////////////////////// -->
 
 ## Looking Ahead: Terms for Next Unit
 
 Research and define the following terms. Hint: use the syllabus!
 
-1. Objects:
+1. Objects: data types within javascrpt that has a collection of keyvalue pairs
 
-2. Classes:
+2. Classes: are the b;lupringts for objects
 
-3. React:
+3. React:system inwhich you see your code real time
